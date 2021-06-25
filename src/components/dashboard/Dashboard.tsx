3a1e17308bd1b5e3785abeca041a841b38f0230e
Ryan Lavin <rlavin@usc.edu>
2021-06-25 11:35:42 -0500

    Dashboard table header started

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -18,9 +18,11 @@ const Dashboard = () => {
     const history = useHistory();
     const dispatch = useDispatch();
 
-    const handleClick = () => {}
+    const handleClick = () => {
+        history.push('/portfolio')
+    }
 
-    function loadPortfolio() {
+    const loadPortfolio = () => {
         history.push('/portfolio')
     }
 
@@ -30,10 +32,71 @@ const Dashboard = () => {
                 isMobile={false}
                 onClickDashboard={loadPortfolio}
             />
-            Dashboard
             <div>
                 <Button onClick={handleClick}>Create Index</Button>
-
+            <div className="table0">
+                <colgroup>
+                    <col style={{width: '36px'}} />
+                    <col style={{width: '50px'}} />
+                    <col style={{width: '250px'}} />
+                    <col style={{width: '170px'}} />
+                    <col style={{width: '100px'}} />
+                    <col style={{width: '100px'}} />
+                    <col style={{width: '100px'}} />
+                    <col style={{width: '175px'}} />
+                    <col style={{width: '150px'}} />
+                </colgroup>
+                <thead>
+                    <tr>
+                        <th className="table0-stickyTop" style={{textAlign: 'left'}}>
+                            <div className="table0-stickyTop-fundId">
+                                <p className="table0-stickyTop-text">Fund ID</p>
+                            </div>
+                        </th>
+                        <th className="table0-stickyTop" style={{textAlign: 'left'}} >
+                            <div className="table0-stickyTop-sortBox">
+                                <p className="sortNum">#</p>
+                                <span className="caretUp"></span>
+                            </div>
+                        </th>
+                        <th className="table0-stickyTop" style={{textAlign: 'left'}}>
+                            <div className="table0-stickyTop-fundName">
+                                <p className="table0-stickyTop-text">Fund Name</p>
+                            </div>
+                        </th>
+                        <th className="table0-stickyTop" style={{textAlign: 'left'}}>
+                            <div className="table0-stickyTop-invested">
+                                <p className="table0-stickyTop-text">Total Value Invested</p>
+                            </div>
+                        </th>
+                        <th className="table0-stickyTop" style={{textAlign: 'left'}}>
+                            <div className="table0-stickyTop-invested">
+                                <p className="table0-stickyTop-text">% Change</p>
+                            </div>
+                        </th>
+                        <th className="table0-stickyTop" style={{textAlign: 'left'}}>
+                            <div className="table0-stickyTop-invested">
+                                <p className="table0-stickyTop-text">24h Change %</p>
+                            </div>
+                        </th>
+                        <th className="table0-stickyTop" style={{textAlign: 'left'}}>
+                            <div className="table0-stickyTop-profit">
+                                <p className="table0-stickyTop-text">Total Loss/Gain</p>
+                            </div>
+                        </th>
+                        <th className="table0-stickyTop" style={{textAlign: 'left'}}>
+                            <div className="table0-stickyTop-nav">
+                                <p className="table0-stickyTop-text">Fund's Total NAV</p>
+                            </div>
+                        </th>
+                        <th className="table0-stickyTop" style={{textAlign: 'left'}}>
+                            <div className="table0-stickyTop-diversity">
+                                <p className="table0-stickyTop-text">Portfolio Diversity</p>
+                            </div>
+                        </th>
+                    </tr>
+                </thead>
+            </div>
                 <br /><br /><br />
             </div>
         </div>
