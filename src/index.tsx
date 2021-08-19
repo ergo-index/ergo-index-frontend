7923ce38d4cdbe0195074234bba43b2a03d631aa
Jordan Bailey <bailey8@buffalo.edu>
2021-08-19 02:15:51 -0400

    Installed and configured tailwind
    tailwind uses postCSS which means normal css files not scss
    Used https://tailwindcss.com/docs/guides/create-react-app

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import App from './app/App';
 import store from './state/store';
 
-import './styles/index.scss'
+import './styles/index.css';
 
 ReactDOM.render(
   <Provider store={store}>
